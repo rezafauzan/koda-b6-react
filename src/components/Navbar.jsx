86baf57c1033ed6e3aae85b9fb64be0292bcd4d8
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-28 00:16:07 +0700

    Feat: add props on Navbar to set position absolute or relative; add Home components; add hero section on home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,25 @@ import brand_white from "../assets/img/brand-white.svg"
 import search_icon from "../assets/img/Search.svg"
 import cart_icon from "../assets/img/ShoppingCart.svg"
 
-const navbar = () => {
+/**
+ * @param {"dark"?} props.theme 
+ * @returns {JSX}
+ */
+const navbar = ({theme,absolute}) => {
     return (
-        <header>
-            <nav className="flex items-center justify-between h-16 px-16 bg-black/40">
+        <header className="relative">
+            <nav className={(absolute?"absolute ":"") + "flex  w-full items-center justify-between h-16 px-16 bg-black/40"}>
                 <ul className="flex items-center gap-4">
                     <li className="hover:text-white"><img src={brand_white} alt="Coffee Shop" /></li>
-                    <li className="hover:text-white border-b border-(--color-primary)"><a href="/">Home</a></li>
-                    <li className="hover:text-white"><a href="/product">Product</a></li>
-                    <li className="hover:text-white"></li>
+                    <li className="hover:text-white text-white border-b border-(--color-primary) hidden md:block"><a href="/">Home</a></li>
+                    <li className="hover:text-white hidden md:block"><a href="/product">Product</a></li>
+                    <li className="hover:text-white hidden md:block"></li>
                 </ul>
                 <ul className="flex items-center gap-4">
                     <li className="hover:text-white"><button className="cursor-pointer"><img src={search_icon} alt="Search_Icon" /></button></li>
                     <li className="hover:text-white"><button className="cursor-pointer"><img src={cart_icon} alt="Cart_Icon" /></button></li>
-                    <li className="text-white hover:text-black"><a href="/login" className="p-4 border border-white rounded">Sign In</a></li>
-                    <li className="hover:text-white"><a href="/register" className="p-4 bg-(--color-primary) border border-(--color-primary) rounded">Sign Up</a></li>
+                    <li className="text-white hover:text-black hidden md:block"><a href="/login" className="p-4 border border-white rounded">Sign In</a></li>
+                    <li className="hover:text-white hidden md:block"><a href="/register" className="p-4 bg-(--color-primary) border border-(--color-primary) rounded">Sign Up</a></li>
                 </ul>
             </nav>
         </header>)
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,9 +2,7 @@ const Section = ({children}) => {
     return (
         <section>
             <div className="section-body">
-                <div className="container flex flex-col min-h-screen md:flex-row">
                     {children}
-                </div>
             </div>
         </section>
     )
