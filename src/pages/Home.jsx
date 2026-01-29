ed456286765180e9b725b0a04edbe8686fd1bd68
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-29 09:48:05 +0700

    Feat: add event to FloatingChat Button to toggle chatbox hidden and not hidden; fix static assets call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import { FaFacebookF } from "react-icons/fa";
 import Navbar from "../components/Navbar"
 import ProductCard from "../components/ProductCard"
 import { MdOutlineGirl } from "react-icons/md";
+import { useRef } from "react";
 
 const Hero = () => {
     return (
@@ -184,9 +185,13 @@ const Footer = () => {
 }
 
 const FloatingChat = () => {
+    const chatbox = useRef()
+    function chatboxToggler(){
+        chatbox.current.classList.toggle('hidden')
+    }
     return (
-        <div className="w-100 flex flex-col justify-center gap-2 fixed bottom-4 right-4 border-t-10 border-t-(--color-primary) rounded-xl">
-            <div className=" bg-white rounded-b">
+        <div className="w-100 flex flex-col justify-center gap-2 fixed bottom-4 right-4">
+            <div ref={chatbox} className=" bg-white rounded-b border-t-10 border-t-(--color-primary) rounded-xl hidden">
                 <div className="flex items-center gap-4 border-b border-b-black/10 p-4">
                     <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center">
                         <AiOutlineUser />
@@ -244,13 +249,13 @@ const FloatingChat = () => {
                     <div className="flex flex-row-reverse justify-self-end items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><AiOutlineUser /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/kiss-cut-stickers-4x4-default-63863ac6746cb_1200x.webp" alt="Koda-Easter-Egg1" className="w-40 h-40" />
+                            <img src="/assets/img/kiss-cut-stickers-4x4-default-63863ac6746cb_1200x.webp" alt="Koda-Easter-Egg1" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><MdOutlineGirl /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/wojak-nss-magazine-3.jpg" alt="Koda-Easter-Egg2" className="w-40 h-40" />
+                            <img src="/assets/img/wojak-nss-magazine-3.jpg" alt="Koda-Easter-Egg2" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex flex-row-reverse justify-self-end items-center gap-4 p-2">
@@ -260,13 +265,13 @@ const FloatingChat = () => {
                     <div className="flex flex-row-reverse justify-self-end items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><AiOutlineUser /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/st,small,845x845-pad,1000x1000,f8f8f8.u2.jpg" alt="Koda-Easter-Egg3" className="w-40 h-40" />
+                            <img src="/assets/img/st,small,845x845-pad,1000x1000,f8f8f8.u2.jpg" alt="Koda-Easter-Egg3" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex flex-row-reverse justify-self-end items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><AiOutlineUser /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/pngimg.com - wojak_PNG109612.png" alt="Koda-Easter-Egg4" className="w-40 h-40" />
+                            <img src="/assets/img/pngimg.com - wojak_PNG109612.png" alt="Koda-Easter-Egg4" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex items-center gap-4 p-2">
@@ -280,13 +285,13 @@ const FloatingChat = () => {
                     <div className="flex flex-row-reverse justify-self-end items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><AiOutlineUser /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/dd3ba633eb5fe0708f057771047d0774.jpg" alt="Koda-Easter-Egg" className="w-40 h-40" />
+                            <img src="/assets/img/dd3ba633eb5fe0708f057771047d0774.jpg" alt="Koda-Easter-Egg" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex items-center gap-4 p-2">
                         <div className="rounded-full w-10 h-10 bg-sky-400 flex justify-center items-center"><MdOutlineGirl /></div>
                         <span className="flex justify-center text-sm bg-slate-100 px-2 py-2">
-                            <img src="/public/assets/img/mygirl.jpg" alt="Koda-Easter-Egg6" className="w-40 h-40" />
+                            <img src="/assets/img/mygirl.jpg" alt="Koda-Easter-Egg6" className="w-40 h-40" />
                         </span>
                     </div>
                     <div className="flex items-center gap-4 p-2">
@@ -303,7 +308,7 @@ const FloatingChat = () => {
                     </button>
                 </div>
             </div>
-            <button className="cursor-pointer flex justify-center items-center bg-(--color-primary) w-16 h-16 rounded-full self-end"><BsChatDots className="w-7 h-7" /></button>
+            <button className="cursor-pointer flex justify-center items-center bg-(--color-primary) w-16 h-16 rounded-full self-end" onClick={chatboxToggler}><BsChatDots className="w-7 h-7" /></button>
         </div>
     )
 }
