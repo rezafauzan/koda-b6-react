d0227a40bb13af37a29678d9217986d4f5912e7e
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-31 23:57:07 +0700

    Feat: add new component <Input /> input has icon and label; change input in Auth pages to <Input />

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import { AiFillEyeInvisible } from "react-icons/ai"; 
+const Input = ({name="name", type="text", icon="", placeholder="", ...attr}) => {
+    return (
+        <>
+            <label htmlFor={name.toLowerCase().replaceAll(" ", "")} className="font-bold text-black">{name}</label>
+            <label className="flex items-center gap-4 p-4 border rounded border-black/40">
+                {(icon ? <img src={icon} alt={name+"_Icon"} /> : "")}
+                <input type={type} name={name.toLowerCase().replaceAll(" ", "")} id={name.toLowerCase().replaceAll(" ", "")} placeholder={placeholder} className="flex-1 outline-0" {...attr}/>
+                {(type === "password" ? <button type="button" className="cursor-pointer"><AiFillEyeInvisible /></button> : "")}
+            </label>
+        </>
+    )
+}
+export default Input
