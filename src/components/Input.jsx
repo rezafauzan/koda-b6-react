e05885a6990e45d367fe8b70924818eb738e14eb
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-02-01 21:50:44 +0700

    Feat: add new field in register phone and address; add assets for phone input; add react-form-hook and forward the ref to <Input />; add yup validation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import { AiFillEyeInvisible } from "react-icons/ai"; 
-const Input = ({name="name", type="text", icon="", placeholder="", label = true, ...attr}) => {
+const Input = ({labelName="labelName", type="text", icon="", placeholder="", label = true, ...attr}, ref) => {
     return (
         <>
-            {(label ? <label htmlFor={name.toLowerCase().replaceAll(" ", "")} className="font-bold text-black">{name}</label> : "")}
+            {(label ? <label htmlFor={labelName.toLowerCase().replaceAll(" ", "")} className="font-bold text-black">{labelName}</label> : "")}
             <label className="flex items-center gap-4 p-4 border rounded border-black/40">
-                {(icon ? <img src={icon} alt={name+"_Icon"} /> : "")}
-                <input type={type} name={name.toLowerCase().replaceAll(" ", "")} id={name.toLowerCase().replaceAll(" ", "")} placeholder={placeholder} className="flex-1 outline-0" {...attr}/>
+                {(icon ? <img src={icon} alt={labelName+"_Icon"} /> : "")}
+                <input ref={ref} type={type} id={labelName.toLowerCase().replaceAll(" ", "")} placeholder={placeholder} className="flex-1 outline-0" {...attr}/>
                 {(type === "password" ? <button type="button" className="cursor-pointer"><AiFillEyeInvisible /></button> : "")}
             </label>
         </>
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,28 +1,55 @@
-import profile_icon from "../../assets/img/Profile.svg"
-import mail_icon from "../../assets/img/mail.svg"
-import password_icon from "../../assets/img/Password.svg"
-import password_show_icon from "../../assets/img/EyeSlash.svg"
-import brand_logo from "../../assets/img/brand.svg"
-import facebook_logo from "../../assets/img/bx_bxl-facebook-circle.svg"
-import google_logo from "../../assets/img/flat-color-icons_google.svg"
-import Input from "../../components/Input"
+import profile_icon from "/src/assets/img/Profile.svg"
+import mail_icon from "/src/assets/img/mail.svg"
+import password_icon from "/src/assets/img/Password.svg"
+import brand_logo from "/src/assets/img/brand.svg"
+import facebook_logo from "/src/assets/img/bx_bxl-facebook-circle.svg"
+import google_logo from "/src/assets/img/flat-color-icons_google.svg"
+import phone_icon from "/src/assets/img/PhoneCall.svg"
+import location_icon from "/src/assets/img/Location.svg"
+import Input from "/src/components/Input"
+import { registerValidation } from "/src/lib/authLogic"
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 
 const Register = () => {
+    const validator = yup.object({
+        fullname: yup.string("Nama tidak valid").required("Nama harus diisi").min(4, "Nama lengkap minimal 4 karakter"),
+        email: yup.string("Email tidak valid").required("Email harus diisi").min(4, "Email terlalu pendek").email("Email tidak valid"),
+        phone: yup.string("Nomor telepon tidak valid").required("Nomor telepon harus diisi").min(10, "Nomor telepon terlalu pendek minimal 10 digit"),
+        address: yup.string("Alamat tidak valid").required("Alamat harus diisi").min(10, "Alamat terlalu pendek minimal 10 karakter"),
+        password: yup.string("Password tidak valid").required("Password harus diisi").min(8, "Password minimal 8 karakter"),
+        confirmPassword: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi").oneOf([yup.ref("password")], "Konfirmasi password tidak cocok"),
+    })
+    const { register, handleSubmit, formState } = useForm({resolver: yupResolver(validator)})
+    
+    function submit(data){
+        registerValidation(data)
+    }
+
     return (
         <section>
             <div className="flex gap-4 w-full">
                 <div className="hidden md:block bg-[url('../../assets/img/register.jpg')] bg-cover bg-center flex-1"></div>
                 <div className="w-full flex flex-col justify-center items-center gap-4 p-10 flex-3 text-(--color-secondary)">
-                    <form className="flex w-full flex-col justify-center gap-4 p-4 flex-3">
+                    <form className="flex w-full flex-col justify-center gap-4 p-4 flex-3" onSubmit={handleSubmit(submit)}>
                         <div className="brand">
                             <img src={brand_logo} alt="Coffee Shop" />
                         </div>
                         <h1 className="font-bold text-(--color-accent)">Register</h1>
                         <span>Fill out the form correctly</span>
-                        <Input type="text" name="Fullname" icon={profile_icon} placeholder="Enter your fullname" />
-                        <Input type="email" name="Email" icon={mail_icon} placeholder="Enter email address" />
-                        <Input type="password" name="Password" icon={password_icon} placeholder="Enter your password" />
-                        <Input type="password" name="Confirm Password" icon={password_icon} placeholder="Confirm your password" />
+                        <Input type="text" {...register("fullname")} labelName="Fullname" icon={profile_icon} placeholder="Enter your fullname" required />
+                        {formState.errors.fullname && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.fullname.message}</span>)}
+                        <Input type="email" {...register("email")} labelName="Email" icon={mail_icon} placeholder="Enter your email address" required />
+                        {formState.errors.email && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.email.message}</span>)}
+                        <Input type="text" {...register("phone")} labelName="Phone" icon={phone_icon} placeholder="Enter your phone number" required />
+                        {formState.errors.phone && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.phone.message}</span>)}
+                        <Input type="text" {...register("address")} labelName="Address" icon={location_icon} placeholder="Enter your address" required />
+                        {formState.errors.address && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.address.message}</span>)}
+                        <Input type="password" {...register("password")} labelName="Password" icon={password_icon} placeholder="Enter your password" required autoComplete="off" />
+                        {formState.errors.password && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.password.message}</span>)}
+                        <Input type="password" {...register("confirmPassword")} labelName="Confirm Password" icon={password_icon} placeholder="Confirm your password" required autoComplete="off" />
+                        {formState.errors.confirmPassword && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.confirmPassword.message}</span>)}
                         <button className="bg-(--color-primary) text-black p-4 rounded cursor-pointer">Register</button>
                         <span className="self-center">have an account? <a href="/login" className="text-(--color-primary)">Login</a></span>
                         <span className="self-center">or</span>
