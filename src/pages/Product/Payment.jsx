4e8c31f74e6e0358450cc85a943415446013065e
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-02-01 18:49:04 +0700

    Feat: add responsive layout for order card

--- src/pages/Product/Payment.jsx
+++ src/pages/Product/Payment.jsx
@@ -15,66 +15,66 @@ const Payment = () => {
                             </div>
                             <div className="flex flex-col gap-4 items-center justify-center">
                                 <div className="flex-1 flex flex-col gap-4">
-                                    <div className="w-full flex rounded overflow-hidden bg-gray-100 px-4">
+                                    <div className="w-full flex flex-col md:flex-row rounded overflow-hidden bg-gray-100 px-4 relative">
                                         {/* {(flashsale ? : '')} */}
                                         <div className="overflow-hidden flex-1 h-full">
                                             <img src="https://placehold.co/1024x1024" alt="Product_Name" className="h-full" />
                                         </div>
-                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3 relative">
+                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3">
                                             <div className="w-fit h-4 p-4 text-white bg-red-700 top-4 left-4 flex flex-col justify-center items-center rounded-full"><span>Flash Sale</span></div>
                                             <h3 className="text-xl font-bold">Hazzelnut Latte</h3>
                                             <span className="text-black/70">2pcs | Regular | Ice | Dine In</span>
                                             {/* <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * discount)},-</span> */}
                                             <span className="text-xl bold text-(--color-primary)">{(true ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * 10 / 100)},-</span>
-                                            <button className="absolute flex justify-center items-center top-[50%] right-4 bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
+                                            <button className="absolute flex justify-center items-center top-0 right-4 md:top-[50%] md:translate-y-[-50%] md:right-4 md:bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex-1 flex flex-col gap-4">
-                                    <div className="w-full flex rounded overflow-hidden bg-gray-100 px-4">
+                                    <div className="w-full flex flex-col md:flex-row rounded overflow-hidden bg-gray-100 px-4 relative">
                                         {/* {(flashsale ? : '')} */}
                                         <div className="overflow-hidden flex-1 h-full">
                                             <img src="https://placehold.co/1024x1024" alt="Product_Name" className="h-full" />
                                         </div>
-                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3 relative">
+                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3">
                                             <div className="w-fit h-4 p-4 text-white bg-red-700 top-4 left-4 flex flex-col justify-center items-center rounded-full"><span>Flash Sale</span></div>
                                             <h3 className="text-xl font-bold">Hazzelnut Latte</h3>
                                             <span className="text-black/70">2pcs | Regular | Ice | Dine In</span>
                                             {/* <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * discount)},-</span> */}
                                             <span className="text-xl bold text-(--color-primary)">{(true ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * 10 / 100)},-</span>
-                                            <button className="absolute flex justify-center items-center top-[50%] right-4 bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
+                                            <button className="absolute flex justify-center items-center top-0 right-4 md:top-[50%] md:translate-y-[-50%] md:right-4 md:bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex-1 flex flex-col gap-4">
-                                    <div className="w-full flex rounded overflow-hidden bg-gray-100 px-4">
+                                    <div className="w-full flex flex-col md:flex-row rounded overflow-hidden bg-gray-100 px-4 relative">
                                         {/* {(flashsale ? : '')} */}
                                         <div className="overflow-hidden flex-1 h-full">
                                             <img src="https://placehold.co/1024x1024" alt="Product_Name" className="h-full" />
                                         </div>
-                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3 relative">
+                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3">
                                             <div className="w-fit h-4 p-4 text-white bg-red-700 top-4 left-4 flex flex-col justify-center items-center rounded-full"><span>Flash Sale</span></div>
                                             <h3 className="text-xl font-bold">Hazzelnut Latte</h3>
                                             <span className="text-black/70">2pcs | Regular | Ice | Dine In</span>
                                             {/* <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * discount)},-</span> */}
                                             <span className="text-xl bold text-(--color-primary)">{(true ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * 10 / 100)},-</span>
-                                            <button className="absolute flex justify-center items-center top-[50%] right-4 bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
+                                            <button className="absolute flex justify-center items-center top-0 right-4 md:top-[50%] md:translate-y-[-50%] md:right-4 md:bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex-1 flex flex-col gap-4">
-                                    <div className="w-full flex rounded overflow-hidden bg-gray-100 px-4">
+                                    <div className="w-full flex flex-col md:flex-row rounded overflow-hidden bg-gray-100 px-4 relative">
                                         {/* {(flashsale ? : '')} */}
                                         <div className="overflow-hidden flex-1 h-full">
                                             <img src="https://placehold.co/1024x1024" alt="Product_Name" className="h-full" />
                                         </div>
-                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3 relative">
+                                        <div className="h-fit flex flex-col gap-4 left-4 right-4 bottom-0 p-4 rounded flex-3">
                                             <div className="w-fit h-4 p-4 text-white bg-red-700 top-4 left-4 flex flex-col justify-center items-center rounded-full"><span>Flash Sale</span></div>
                                             <h3 className="text-xl font-bold">Hazzelnut Latte</h3>
                                             <span className="text-black/70">2pcs | Regular | Ice | Dine In</span>
                                             {/* <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * discount)},-</span> */}
                                             <span className="text-xl bold text-(--color-primary)">{(true ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * 10 / 100)},-</span>
-                                            <button className="absolute flex justify-center items-center top-[50%] right-4 bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
+                                            <button className="absolute flex justify-center items-center top-0 right-4 md:top-[50%] md:translate-y-[-50%] md:right-4 md:bottom-[50%] h-10 w-10 cursor-pointer"><CgCloseO className="h-10 w-10 text-red-700" /></button>
                                         </div>
                                     </div>
                                 </div>
