b8b0384338947ada3bcc106662dd6d647e5100e2
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-02-01 18:04:02 +0700

    Fix: wrong position between base price and after discount price

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,7 +10,7 @@ const ProductCard = ({ flashsale = false, discount = 10 / 100 }) => {
             <div className="h-fit flex flex-col gap-4 left-4 right-4 absolute bottom-0 p-4 bg-white shadow-lg rounded">
                 <h3 className="text-xl font-bold">Hazzelnut Latte</h3>
                 <p>You can explore the menu that we provide with fun and have their own taste and make your day better.</p>
-                <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs"> Rp.{22000 - (22000 * discount)},- </span> : "")} Rp.22000,-</span>
+                <span className="text-xl bold text-(--color-primary)">{(flashsale ? <span className="text-red-700 line-through text-xs">Rp.22000,-</span> : "")} Rp.{22000 - (22000 * discount)},-</span>
                 <div className="flex flex-col md:flex-row gap-4">
                     <a href="/product/0" className="flex-4 px-4 py-2 bg-(--color-primary) hover:bg-(--color-primary-active) rounded flex justify-center items-center cursor-pointer">Buy</a>
                     <button className="px-4 py-2 border border-(--color-primary) rounded flex-1 flex justify-center items-center cursor-pointer">
