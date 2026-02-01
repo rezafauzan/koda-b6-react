4867581821f068819a89ea82e2827b8aa94ca56d
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-02-02 01:07:12 +0700

    Feat: add register logic save users detail to local storage

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -11,20 +11,39 @@ import { registerValidation } from "/src/lib/authLogic"
 import { useForm } from "react-hook-form"
 import * as yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup"
+import { useEffect, useState } from "react"
 
 const Register = () => {
+    const [users, setUsers] = useState([])
+    useEffect(
+        () => {
+            const usersLocalStorage = JSON.parse(localStorage.getItem("users")) || []
+            setUsers(usersLocalStorage)
+        },
+        []
+    )
     const validator = yup.object({
-        fullname: yup.string("Nama tidak valid").required("Nama harus diisi").min(4, "Nama lengkap minimal 4 karakter"),
+        fullname: yup.string("Nama tidak valid"),
         email: yup.string("Email tidak valid").required("Email harus diisi").min(4, "Email terlalu pendek").email("Email tidak valid"),
         phone: yup.string("Nomor telepon tidak valid").required("Nomor telepon harus diisi").min(10, "Nomor telepon terlalu pendek minimal 10 digit"),
         address: yup.string("Alamat tidak valid").required("Alamat harus diisi").min(10, "Alamat terlalu pendek minimal 10 karakter"),
         password: yup.string("Password tidak valid").required("Password harus diisi").min(8, "Password minimal 8 karakter"),
-        confirmPassword: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi").oneOf([yup.ref("password")], "Konfirmasi password tidak cocok"),
+        confirmPassword: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi"),
     })
-    const { register, handleSubmit, formState } = useForm({resolver: yupResolver(validator)})
-    
-    function submit(data){
-        registerValidation(data)
+    const { register, handleSubmit, formState } = useForm({ resolver: yupResolver(validator) })
+    function submit({ fullname = "", email = "", phone = "", address = "", password = "", confirmPassword = "" }) {
+        const registeredUsers = users
+        registeredUsers.push(
+            {
+                fullname: fullname,
+                email: email,
+                phone: phone,
+                address: address,
+                password: password,
+                confirmPassword: confirmPassword
+            }
+        )
+        setUsers(registeredUsers)
     }
 
     return (
@@ -38,7 +57,7 @@ const Register = () => {
                         </div>
                         <h1 className="font-bold text-(--color-accent)">Register</h1>
                         <span>Fill out the form correctly</span>
-                        <Input type="text" {...register("fullname")} labelName="Fullname" icon={profile_icon} placeholder="Enter your fullname" required />
+                        <Input type="text" {...register("fullname")} labelName="Fullname" icon={profile_icon} placeholder="Enter your fullname" />
                         {formState.errors.fullname && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.fullname.message}</span>)}
                         <Input type="email" {...register("email")} labelName="Email" icon={mail_icon} placeholder="Enter your email address" required />
                         {formState.errors.email && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.email.message}</span>)}
