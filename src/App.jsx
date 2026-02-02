6849c5549f960937b5f9282785d8db382710521a
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-02-02 11:11:11 +0700

    Feat: ProductContext now only wrap outlet from ProductLayout Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import ForgotPassword from './pages/Auth/Forgot'
 import ProductLayout from './pages/Product/ProductLayout'
 import Product from './pages/Product/Product'
 import { useEffect, useState } from 'react'
-import ProductContext from './components/context/ProductContext'
-import dataFetcher from './lib/dataFetcher'
 import ProductDetail from './pages/Product/ProductDetail'
 import PaymentLayout from './pages/Payment/PaymentLayout'
 import Payment from './pages/Payment/Payment'
@@ -15,13 +13,9 @@ import OrderHistory from './pages/Payment/OrderHistory'
 import UserContext from './components/context/UserContext'
 
 const App = () => {
-    const [data, setData] = useState([])
     const [user, setUser] = useState({})
     useEffect(
         () => {
-            dataFetcher("https://raw.githubusercontent.com/rezafauzan/koda-b6-react/refs/heads/feat/product-detail/public/assets/data/product.json").then(
-                products => { setData(products) }
-            )
             const usersLocalStorage = JSON.parse(localStorage.getItem("user")) || []
             setUser(usersLocalStorage)
         }
@@ -76,9 +70,7 @@ const App = () => {
     ])
     return (
         <UserContext value={user}>
-            <ProductContext value={data}>
                 <RouterProvider router={router} />
-            </ProductContext>
         </UserContext>
     )
 }
