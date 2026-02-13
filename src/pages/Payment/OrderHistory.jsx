62a91de9e45b51424fb089c9c9064e29b894f989
rezafauzan <rezafauzan593@gmail.com>
2026-02-13 14:27:58 +0700

    Feat: Filter Order History by order status

--- src/pages/Payment/OrderHistory.jsx
+++ src/pages/Payment/OrderHistory.jsx
@@ -2,12 +2,22 @@ import { BsFillChatLeftTextFill } from "react-icons/bs";
 import Input from "/src/components/Input.jsx";
 import { Link } from "react-router-dom";
 import useLocalStorage from "../../hooks/useLocalStorage"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ProductContext from "../../components/context/ProductContext"
+import { useForm } from "react-hook-form";
 
 const Payment = () => {
     const [histories, setHistories] = useLocalStorage("history-order")
+    const [historiesOrder, setHistoriesOrder] = useState(histories)
     const productsData = useContext(ProductContext)
+    const {register, handleSubmit} = useForm()
+
+    function filterHistoryOrder({status, date}) {
+        setHistoriesOrder(histories)
+        const filteredHistories = histories.filter(history=>history.status === parseInt(status))
+        setHistoriesOrder(filteredHistories)
+    }
+
     return (
         <section>
             <div className="grid grid-cols-1 md:grid-cols-2">
@@ -19,31 +29,31 @@ const Payment = () => {
                         <div className="flex flex-col lg:flex-row gap-4 p-4 flex-2">
                             <div className="flex-1 flex flex-col gap-4 w-full">
                                 <div className="flex flex-col gap-4">
-                                    <div className="flex flex-col md:flex-row gap-4 justify-between items-center">
+                                    <form className="flex flex-col md:flex-row gap-4 justify-between items-center" onSubmit={handleSubmit(filterHistoryOrder)} onChange={e=>e.currentTarget.requestSubmit()}>
                                         <div className="flex flex-3 gap-4 justify-center items-center bg-gray-100 rounded">
-                                            <label htmlFor="onProgress" className="group flex-1 flex justify-center items-center rounded">
-                                                <input type="radio" labelName="order" id="onProgress" className="hidden" />
-                                                <div className="w-full group-has-[input:checked]:bg-gray-400 flex flex-col p-4 justify-center items-center rounded flex-1 hover:border-(--color-primary-active) cursor-pointer ">
+                                            <label htmlFor="onProgress" className="group flex-1 flex justify-center items-center">
+                                                <input type="radio" {...register("status")} id="onProgress" value={0} className="hidden"/>
+                                                <div className="w-full group-has-[input:checked]:bg-gray-400 group-has-[input:checked]:text-white flex flex-col p-4 justify-center items-center border border-black/40 rounded flex-1 hover:border-gray-400 hover:bg-gray-400 hover:text-white cursor-pointer">
                                                     <span>On Progress</span>
                                                 </div>
                                             </label>
-                                            <label htmlFor="sendingGoods" className="group flex-1 flex justify-center items-cente roundedr">
-                                                <input type="radio" labelName="order" id="sendingGoods" className="hidden" />
-                                                <div className="w-full group-has-[input:checked]:bg-gray-400 flex flex-col p-4 justify-center items-center rounded flex-1 hover:border-(--color-primary-active) cursor-pointer ">
+                                            <label htmlFor="sendingGoods" className="group flex-1 flex justify-center items-center">
+                                                <input type="radio" {...register("status")} id="sendingGoods" value={1} className="hidden"/>
+                                                <div className="w-full group-has-[input:checked]:bg-gray-400 group-has-[input:checked]:text-white flex flex-col p-4 justify-center items-center border border-black/40 rounded flex-1 hover:border-gray-400 hover:bg-gray-400 hover:text-white cursor-pointer">
                                                     <span>Sending Goods</span>
                                                 </div>
                                             </label>
-                                            <label htmlFor="finishOrder" className="group flex-1 flex justify-center items-center rounded">
-                                                <input type="radio" labelName="order" id="finishOrder" className="hidden" />
-                                                <div className="w-full group-has-[input:checked]:bg-gray-400 flex flex-col p-4 justify-center items-center rounded flex-1 hover:border-(--color-primary-active) cursor-pointer ">
+                                            <label htmlFor="finishOrder" className="group flex-1 flex justify-center items-center">
+                                                <input type="radio" {...register("status")} id="finishOrder" value={2} className="hidden"/>
+                                                <div className="w-full group-has-[input:checked]:bg-gray-400 group-has-[input:checked]:text-white flex flex-col p-4 justify-center items-center border border-black/40 rounded flex-1 hover:border-gray-400 hover:bg-gray-400 hover:text-white cursor-pointer">
                                                     <span>Finish Order</span>
                                                 </div>
                                             </label>
                                         </div>
                                         <div className="flex flex-1 gap-4 justify-center items-center">
-                                            <Input type="date" labelName="orderByDate" label={false} className="w-full" />
+                                            <Input type="date" {...register("date")} labelName="orderByDate" label={false} className="w-full" />
                                         </div>
-                                    </div>
+                                    </form>
                                 </div>
                                 <div className="flex flex-col w-full gap-4 items-center h-128 p-4 overflow-y-auto">
                                     <div className="flex-1 w-full flex flex-col gap-4">
@@ -51,9 +61,9 @@ const Payment = () => {
                                             (
                                                 productsData != null
                                                     ?
-                                                    histories != null
+                                                    historiesOrder != null
                                                         ?
-                                                        histories.map(
+                                                        historiesOrder.map(
                                                             history => {
                                                                 return (
                                                                     <div className="w-full h-fit md:h-45 flex flex-col md:flex-row justify-center md:justify-start items-center md:items-start text-center md:text-start rounded overflow-hidden bg-gray-100 p-4">
@@ -61,20 +71,20 @@ const Payment = () => {
                                                                             <img src={productsData[parseInt(history.cart[0].productId)].images[0]} alt={productsData[parseInt(history.cart[0].productId)].name} className="h-full" />
                                                                         </div>
                                                                         <div className="h-fit flex flex-col md:flex-row gap-4 p-4 rounded flex-3">
-                                                                            <div className="flex flex-col gap-4">
+                                                                            <div className="flex flex-col gap-4 text-[14px]">
                                                                                 <h4>No. Order</h4>
                                                                                 <span className="font-bold">#{history.id}</span>
                                                                                 <Link to={"/order/" + history.id} className="text-(--color-primary) underline">Views Order Detail</Link>
                                                                             </div>
-                                                                            <div className="flex flex-col gap-4">
+                                                                            <div className="flex flex-col gap-4 text-[14px]">
                                                                                 <h4>Date</h4>
                                                                                 <span className="font-bold">{history.orderDate}</span>
                                                                             </div>
-                                                                            <div className="flex flex-col gap-4">
+                                                                            <div className="flex flex-col gap-4 text-[14px]">
                                                                                 <h4>Total</h4>
                                                                                 <span className="font-bold">{"Rp." + history.total + ",-"}</span>
                                                                             </div>
-                                                                            <div className="flex flex-col gap-4">
+                                                                            <div className="flex flex-col gap-4 text-[14px]">
                                                                                 <h4>Status</h4>
                                                                                 {
                                                                                     (
@@ -83,14 +93,14 @@ const Payment = () => {
                                                                                             <span className="w-48 md:w-40 flex justify-center items-center font-bold bg-amber-400 text-amber-700 px-4 rounded-full">On Progress</span>
                                                                                             :
                                                                                             history.status === 1
-                                                                                            ?
-                                                                                            <span className="w-48 md:w-40 flex justify-center items-center font-bold bg-blue-400 text-blue-700 px-4 rounded-full">On Delivery</span>
-                                                                                            :
-                                                                                            history.status === 2
-                                                                                            ?
-                                                                                            <span className="w-48 md:w-40 flex justify-center items-center font-bold bg-green-400 text-green-700 px-4 rounded-full">Done</span>
-                                                                                            :
-                                                                                            ""
+                                                                                                ?
+                                                                                                <span className="w-48 md:w-40 flex justify-center items-center font-bold bg-blue-400 text-blue-700 px-4 rounded-full">On Delivery</span>
+                                                                                                :
+                                                                                                history.status === 2
+                                                                                                    ?
+                                                                                                    <span className="w-48 md:w-40 flex justify-center items-center font-bold bg-green-400 text-green-700 px-4 rounded-full">Done</span>
+                                                                                                    :
+                                                                                                    ""
                                                                                     )
                                                                                 }
                                                                             </div>
