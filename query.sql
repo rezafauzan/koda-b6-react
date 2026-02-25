68d9b18d87c279f8546f9bd3bb63995c1eae156a
rezafauzan <rezafauzan593@gmail.com>
2026-02-25 13:12:48 +0700

    Feat: add query to show choosen product with choosen variant and choosen portions and show final price (product.price + variant&portions.additional_price)

---
+++ query.sql
@@ -0,0 +1,2 @@
+-- 1 product agregasikan dengan nama product, variant, size, final price
+SELECT products.name product_name, product_variants.variant_name variant, product_portions.portion_size size, (products.price + product_variants.additional_price + product_portions.additional_price) final_price FROM products JOIN product_variants ON product_variants.product_id = products.id JOIN product_portions ON product_portions.product_id = products.id WHERE products.id = 4 AND product_variants.id = 7 AND product_portions.id = 10;
