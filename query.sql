b30b1344e7ea668f921b084fc1aeb6b559c87c6e
rezafauzan <rezafauzan593@gmail.com>
2026-02-25 14:00:09 +0700

    Feat: add query to show sub total from multiple product with each variant and portion

--- query.sql
+++ query.sql
@@ -1,2 +1,5 @@
 -- 1 product agregasikan dengan nama product, variant, size, final price
+
 SELECT products.name product_name, product_variants.variant_name variant, product_portions.portion_size size, (products.price + product_variants.additional_price + product_portions.additional_price) final_price FROM products JOIN product_variants ON product_variants.product_id = products.id JOIN product_portions ON product_portions.product_id = products.id WHERE products.id = 4 AND product_variants.id = 7 AND product_portions.id = 10;
+
+SELECT products.name product_name, product_variants.variant_name variant, product_portions.portion_size size, (10) quantity,((products.price + product_variants.additional_price + product_portions.additional_price) * 10) price FROM products JOIN product_variants ON product_variants.product_id = products.id JOIN product_portions ON product_portions.product_id = products.id WHERE products.id IN (4,2,1,5) AND product_variants.id IN (7,4,2,9) AND product_portions.id IN (9,5,1,12);
