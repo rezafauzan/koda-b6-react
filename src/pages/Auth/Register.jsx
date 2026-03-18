bd23ba1abd0bf889608767fe7c2e6e97655a2cc1
rezafauzan <rezafauzan593@gmail.com>
2026-03-18 13:50:06 +0700

    Feat: user Register now handle by backend

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -14,10 +14,9 @@ import { useContext, useEffect, useRef, useState } from "react"
 import UserContext from "/src/components/context/UserContext";
 import { Link, useNavigate } from "react-router-dom";
 import AlertContext from "../../components/context/AlertContext";
-import useLocalStorage from "../../hooks/useLocalStorage"
+import http from "../../lib/dataFetcher"
 
 const Register = () => {
-    const [users, setUsers] = useLocalStorage("users")
     const { user } = useContext(UserContext)
     const { setAlert } = useContext(AlertContext)
     const navigator = useNavigate()
@@ -31,58 +30,35 @@ const Register = () => {
         [user]
     )
     const validator = yup.object({
-        fullname: yup.string("Nama tidak valid").required("Nama harus diisi").min(4, "Nama minimal 4 karakter"),
+        // first_name: yup.string("Nama depan tidak valid").required("Nama depan harus diisi").min(4, "Nama depan minimal 4 karakter"),
+        last_name: yup.string("Nama belakang tidak valid").required("Nama belakang harus diisi").min(4, "Nama belakang minimal 4 karakter"),
         email: yup.string("Email tidak valid").required("Email harus diisi").min(4, "Email terlalu pendek").email("Email tidak valid"),
         phone: yup.string("Nomor telepon tidak valid").required("Nomor telepon harus diisi").min(10, "Nomor telepon terlalu pendek minimal 10 digit"),
         address: yup.string("Alamat tidak valid").required("Alamat harus diisi").min(10, "Alamat terlalu pendek minimal 10 karakter"),
         password: yup.string("Password tidak valid").required("Password harus diisi").min(8, "Password minimal 8 karakter"),
-        confirmPassword: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi").oneOf([yup.ref("password")], "Konfirmasi Password tidak sama")
+        password_confirm: yup.string("Konfirmasi Password tidak valid").required("Konfirmasi Password harus diisi").oneOf([yup.ref("password")], "Konfirmasi Password tidak sama")
     })
+
     const { register, handleSubmit, formState } = useForm({ resolver: yupResolver(validator) })
-    function signUp({ fullname = "", email = "", phone = "", address = "", password = "", confirmPassword = "" }) {
-        if(users != null){
-            const registeredUsers = users
-            const emailExist = (users != null ? registeredUsers.filter(user => user.email === email) : 0)
-            if (emailExist.length > 0) {
-                setAlert(['fail', 'Email sudah terdaftar silahkan login'])
-            } else {
-                registeredUsers.push(
-                    {
-                        id: users.length,
-                        avatar: "https://i.pravatar.cc/400?img=54",
-                        fullname: fullname.trim(),
-                        email: email.trim(),
-                        phone: phone.trim(),
-                        address: address.trim(),
-                        password: btoa(password.trim()),
-                        role: "user",
-                        cart: [],
-                        historyOrders: []
-                    }
-                )
-                setUsers(registeredUsers)
-                setAlert(['success', 'Registrasi berhasil silahkan login'])
-                navigator("/login")
-            }
-        }else{
-            const registeredUsers = []
-            registeredUsers.push(
-                {
-                    id: 0,
-                    avatar: "https://i.pravatar.cc/400?img=54",
-                    fullname: fullname.trim(),
-                    email: email.trim(),
-                    phone: phone.trim(),
-                    address: address.trim(),
-                    password: btoa(password.trim()),
-                    role: "user",
-                    cart: [],
-                    historyOrders: []
-                }
-            )
-            setUsers(registeredUsers)
-            setAlert(['success', 'Registrasi berhasil silahkan login'])
+
+    async function signUp({ first_name = "", last_name = "", email = "", phone = "", address = "", password = "", password_confirm = "" }) {
+        const body = {
+            "first_name": first_name.trim().toLowerCase(),
+            "last_name": last_name.trim().toLowerCase(),
+            "email": email.trim().toLowerCase(),
+            "phone": phone.trim().toLowerCase(),
+            "address": address.trim().toLowerCase(),
+            "password": password,
+            "password_confirm": password_confirm
+        }
+        const req = await http("/users", JSON.stringify(body), { method: "POST", headers: { "Content-Type": "application/json" } })
+        const data = await req.json()
+
+        if (data.success) {
+            setAlert(['success', data.messages])
             navigator("/login")
+        } else {
+            setAlert(['fail', data.messages])
         }
     }
 
@@ -97,7 +73,9 @@ const Register = () => {
                         </div>
                         <h1 className="font-bold text-(--color-accent)">Register</h1>
                         <span>Fill out the form correctly</span>
-                        <Input type="text" {...register("fullname")} labelName="Fullname" icon={profile_icon} placeholder="Enter your fullname" />
+                        <Input type="text" {...register("first_name")} labelName="First Name" icon={profile_icon} placeholder="Enter your fullname" />
+                        {formState.errors.fullname && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.fullname.message}</span>)}
+                        <Input type="text" {...register("last_name")} labelName="Last Name" icon={profile_icon} placeholder="Enter your fullname" />
                         {formState.errors.fullname && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.fullname.message}</span>)}
                         <Input type="email" {...register("email")} labelName="Email" icon={mail_icon} placeholder="Enter your email address" required />
                         {formState.errors.email && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.email.message}</span>)}
@@ -107,7 +85,7 @@ const Register = () => {
                         {formState.errors.address && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.address.message}</span>)}
                         <Input type="password" {...register("password")} labelName="Password" icon={password_icon} placeholder="Enter your password" required autoComplete="off" />
                         {formState.errors.password && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.password.message}</span>)}
-                        <Input type="password" {...register("confirmPassword")} labelName="Confirm Password" icon={password_icon} placeholder="Confirm your password" required autoComplete="off" />
+                        <Input type="password" {...register("password_confirm")} labelName="Confirm Password" icon={password_icon} placeholder="Confirm your password" required autoComplete="off" />
                         {formState.errors.confirmPassword && (<span className="bg-red-400 p-4 rounded border border-red-700 text-red-700">{formState.errors.confirmPassword.message}</span>)}
                         <button className="bg-(--color-primary) text-black p-4 rounded cursor-pointer">Register</button>
                         <span className="self-center">have an account? <Link to="/login" className="text-(--color-primary)">Login</Link></span>
