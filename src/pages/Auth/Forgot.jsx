8368e8a46e2d35acd31595d2a13b72c6cadd0309
rezafauzan <rezafauzan593@gmail.com>
2026-03-30 04:44:02 +0700

    Fix: minor issues

--- src/pages/Auth/Forgot.jsx
+++ src/pages/Auth/Forgot.jsx
@@ -30,8 +30,8 @@ const ForgotPassword = () => {
         }
     }
 
-    async function forgot({ email, code_OTP, password, confirm_password }) {
-        if (!email || !code_OTP || !password || !confirm_password) {
+    async function forgot({ remail, code_OTP, password, confirm_password }) {
+        if (!remail || !code_OTP || !password || !confirm_password) {
             setAlert(['fail', 'Semua field wajib diisi'])
             return
         }
@@ -41,13 +41,11 @@ const ForgotPassword = () => {
             return
         }
         const body = {
-            email: email.trim().toLowerCase(),
+            email: remail.trim().toLowerCase(),
             code_otp: code_OTP,
             new_password: password,
             password_confirm: confirm_password
         }
-        console.log(body)
-
         try {
             const req = await http("/forgot-password/reset", JSON.stringify(body), { method: "POST", headers: { "Content-Type": "application/json" } })
             const res = await req.json()
@@ -88,11 +86,11 @@ const ForgotPassword = () => {
                         <form className="flex flex-col justify-center gap-4" onSubmit={handleSubmit(requestOTP)}>
                             <Input type="email" {...register("email")} labelName="Email" icon={mail_icon} placeholder="Enter email address" className="flex-4" required />
                             <div className="flex items-center gap-4">
-                                <button onClick={requestOTP} className="bg-(--color-primary) text-black p-4 rounded cursor-pointer">Request OTP</button>
+                                <button className="bg-(--color-primary) text-black p-4 rounded cursor-pointer">Request OTP</button>
                             </div>
                         </form>
                         <form onSubmit={handleSubmit(forgot)} className="flex flex-col gap-4">
-                            <Input type="email" {...register("email")} labelName="Email" icon={mail_icon} placeholder="Enter email address" className="flex-4" required />
+                            <Input type="email" {...register("remail")} labelName="Email" icon={mail_icon} placeholder="Enter email address" className="flex-4" required />
                             <Input type="text" {...register("code_OTP")} labelName="Code OTP" icon={mail_icon} placeholder="Enter OTP code" required />
                             <Input type="password" {...register("password")} labelName="Password" icon={mail_icon} placeholder="*******" required autoComplete="off" />
                             <Input type="password" {...register("confirm_password")} labelName="Confirm Password" icon={mail_icon} placeholder="********" required autoComplete="off"/>
